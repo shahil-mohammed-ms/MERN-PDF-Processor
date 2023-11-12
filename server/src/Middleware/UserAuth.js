f539e0fd00c1caf3c2aa90c6c6721d485b894d42
shahil-mohammed-ms <115145954+shahil-mohammed-ms@users.noreply.github.com>
2023-11-12 16:04:25 +0530

    error code cleaning

--- server/src/Middleware/UserAuth.js
+++ server/src/Middleware/UserAuth.js
@@ -1,31 +1,4 @@
-// const jwt = require('jsonwebtoken');
-// const { JWT_SECRET } = process.env;
 
-
-// const authenticateToken = async (req,res,next)=>{
-
-//   const token = req.header('Authorization').replace('Bearer ', '');
- 
-//     if (!token) {
-//       return res.status(401).json({ message: 'Unauthorized err' });
-//     }
-  
-    
-  
-//     jwt.verify(token, JWT_SECRET, (err, user) => {
-//       if (err) {
-//         console.log('fail sellauth')
-//         return res.status(403).json({ message: 'Forbidden err' });
-//       }
-     
-//       req.user = user;
-//       next();
-//     });
-
-
-// }
-
-// module.exports=authenticateToken
 const jwt = require('jsonwebtoken');
 const { JWT_SECRET } = process.env;
 console.log(JWT_SECRET);
